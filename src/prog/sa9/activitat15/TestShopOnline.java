83d3b806c9b85fe1842fb181f4a5eb9734902d09
Joan <joamircor@alu.edu.gva.es>
2026-04-17 20:27:51 +0200

    Test Finalizado

--- src/prog/sa9/activitat15/TestShopOnline.java
+++ src/prog/sa9/activitat15/TestShopOnline.java
@@ -51,5 +51,66 @@ public class TestShopOnline {
 
         System.out.println("APARADOR OBERT?");
         System.out.println(aparador.estaObert());
+
+        Producte camisetaPepe = new Producte("camiseta pepe", 25, Marca.PEPEWEAR);
+        Producte pantaloPepe = new Producte("pantalo pepe", 35, Marca.PEPEWEAR);
+
+        ArrayList<Producte> productesPepe = new ArrayList<>();
+        productesPepe.add(camisetaPepe);
+        productesPepe.add(pantaloPepe);
+
+        AparadorFinsFiExistencies aparadorPepe = new AparadorFinsFiExistencies(
+                "outlet PepeWear",
+                Marca.PEPEWEAR,
+                productesPepe
+        );
+
+        System.out.println("APARADOR FINS FI EXISTENCIES OBERT");
+        System.out.println(aparadorPepe.estaObert());
+
+        aparadorPepe.tancar();
+
+        System.out.println("APARADOR FINS FI EXISTENCIES TANCAT");
+        System.out.println(aparadorPepe.estaObert());
+
+        Producte taronger1 = new Producte("camisa taronger", 30, Marca.FLOR_DEL_TARONGER);
+        Producte taronger2 = new Producte("falda taronger", 45, Marca.FLOR_DEL_TARONGER);
+
+        ArrayList<Producte> productesTaronger = new ArrayList<>();
+        productesTaronger.add(taronger1);
+        productesTaronger.add(taronger2);
+
+        AparadorTemporal aparadorTemporal = new AparadorTemporal(
+                "promo primavera",
+                Marca.FLOR_DEL_TARONGER,
+                productesTaronger,
+                5
+        );
+
+        System.out.println("APARADOR TEMPORAL OBERT");
+        System.out.println(aparadorTemporal.estaObert());
+        System.out.println("DIES RESTANTS APARADOR TEMPORAL");
+        System.out.println(aparadorTemporal.getDiesRestants());
+
+        System.out.println("CONSULTA DE PRODUCTE DISPONIBLE");
+        System.out.println(aparadorTemporal.teProducte(taronger1));
+
+        System.out.println("CONSULTA D'UNITATS DISPONIBLES");
+        System.out.println(aparadorTemporal.hiHaUnitatsDisponibles(taronger1, 1));
+
+        /*
+        Producte producteMarcaIncorrecta = new Producte("error marca", 20, Marca.PEPEWEAR);
+        ArrayList<Producte> llistaIncorrecta = new ArrayList<>();
+        llistaIncorrecta.add(producteMarcaIncorrecta);
+        AparadorTemporal errorAparador = new AparadorTemporal("error", Marca.LACOSTERA, llistaIncorrecta, 2);
+        */
+
+        /*
+        try {
+            aparadorTemporal.comprarProducte(taronger1, 999);
+        } catch (ShowRoomNotOpenException e) {
+            System.out.println(e.getMessage());
+        }
+        */
     }
 }
